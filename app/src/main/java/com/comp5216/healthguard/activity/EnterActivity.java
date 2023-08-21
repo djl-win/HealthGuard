378a64945569b0015b6bac90d929f0fecb97a077
Jiale Dong <jdon0433@uni.sydney.edu.au>
2023-08-22 02:34:34 +1000

    init project and finish the ui of enter page

---
+++ app/src/main/java/com/comp5216/healthguard/activity/EnterActivity.java
@@ -0,0 +1,16 @@
+package com.comp5216.healthguard.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.comp5216.healthguard.R;
+
+public class EnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+}
